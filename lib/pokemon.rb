c39f6e0b2a0eea20e370a7b31fe95c27ed4b2c9a
Ryan Erricson <ryanerricson@gmail.com>
2021-03-04 14:02:40 -0500

    commit tree d9f338fc3a4e9cb146ab52dba16602fe0e89f745

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
+require "pry"
+
 class Pokemon
+
+    attr_accessor :name, :type
+    attr_reader :id, :db
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+          INSERT INTO pokemon (name, type) 
+          VALUES (?, ?)
+        SQL
+    
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    
+      end
+      
+
+      def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        result = db.execute(sql, id)[0]
+        Pokemon.new(id:id, name:result[1], type:result[2], db:db)
+      end
+
+    
 end
